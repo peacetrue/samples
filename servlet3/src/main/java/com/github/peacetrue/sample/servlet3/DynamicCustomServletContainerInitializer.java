39ea2698d35dbbedf5f0ffd8d00911f6a6224ae2
xiayx <xiayouxue@gold32.com>
2021-06-27 10:34:03 +0800

    DynamicCustomServletContainerInitializer

--- servlet3/src/main/java/com/github/peacetrue/sample/servlet3/DynamicCustomServletContainerInitializer.java
+++ servlet3/src/main/java/com/github/peacetrue/sample/servlet3/DynamicCustomServletContainerInitializer.java
@@ -3,35 +3,27 @@ package com.github.peacetrue.sample.servlet3;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.DispatcherType;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import java.util.EnumSet;
 
 /**
  * @author : xiayx
- * @since : 2021-06-26 18:13
+ * @since : 2021-06-27 05:20
  **/
 @Slf4j
-@WebListener
-public class DynamicServletContextListener implements ServletContextListener {
-
+public class DynamicCustomServletContainerInitializer implements CustomServletContainerInitializer {
     @Override
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
+    public void onStartup(ServletContext ctx) throws ServletException {
         log.info("动态注册");
         //如果给定的过滤器映射应该在任何声明的过滤器映射之后匹配，则为 true，
         //如果它应该在获得此 FilterRegistration 的 ServletContext 的任何已声明的过滤器映射之前匹配，则为 false
         String filterName = DynamicFilter.class.getName();
-        servletContextEvent.getServletContext()
+        ctx
                 .addFilter(filterName + "After", new DynamicFilter())
                 .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
-        servletContextEvent.getServletContext()
+        ctx
                 .addFilter(filterName + "Before", new DynamicFilter())
                 .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
-    }
-
-    @Override
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        log.info("容器被销毁");
     }
 }
