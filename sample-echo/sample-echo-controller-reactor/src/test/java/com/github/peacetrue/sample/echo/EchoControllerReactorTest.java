76745ee5d672a338c9e793e009cdeeb3219bedfc
peacetrue <xiayouxue@gmail.com>
2022-09-18 09:19:32 +0800

    remove unused JacksonAutoConfiguration

--- sample-echo/sample-echo-controller-reactor/src/test/java/com/github/peacetrue/sample/echo/EchoControllerReactorTest.java
+++ sample-echo/sample-echo-controller-reactor/src/test/java/com/github/peacetrue/sample/echo/EchoControllerReactorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebTestClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +19,6 @@ import org.springframework.test.web.reactive.server.WebTestClient;
         EchoServiceReactorAutoConfiguration.class,
         EchoControllerReactorAutoConfiguration.class,
         WebFluxAutoConfiguration.class,
-        JacksonAutoConfiguration.class,
         WebTestClientAutoConfiguration.class,
 })
 class EchoControllerReactorTest {
